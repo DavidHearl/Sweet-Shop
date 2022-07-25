cfaa1f2e7ce4a292a72b972dae0e74aca9336509
DavidHearl <david.hearl@outlook.com>
2022-07-25 22:51:56 +0100

    Updated the context processor, which updates the shopping back

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -1,9 +1,24 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def shopping_bag_contents(request):
 
     shopping_bag_items = []
     total = 0
     product_count = 0
+    shopping_bag = request.session.get('shopping_bag', {})
+    
+
+    for item_id, quantity in shopping_bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        shopping_bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     grand_total = total
 
     context = {
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from statistics import quantiles
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
@@ -6,3 +7,19 @@ def view_shopping_bag(request):
     """ A view to return the contents of the shopping bag/basket """
     
     return render(request, 'shopping_bag/shopping_bag.html')
+
+
+def add_to_shopping_bag(request, item_id):
+    """ A view to add the quanity to the shopping bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if item_id in list (shopping_bag.keys()):
+        shopping_bag[item_id] += quantity
+    else:
+        shopping_bag[item_id] = quantity
+    
+    request.session['shopping_bag'] = shopping_bag
+    return redirect(redirect_url)
