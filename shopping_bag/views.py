e18885a20233752bb8049e376f2fb00a063245c4
DavidHearl <david.hearl@outlook.com>
2022-07-26 17:14:07 +0100

    Added update and remove in the bag

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,7 +1,5 @@
-from statistics import quantiles
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse, HttpResponse
 
-# Create your views here.
 
 def view_shopping_bag(request):
     """ A view to return the contents of the shopping bag/basket """
@@ -23,3 +21,28 @@ def add_to_shopping_bag(request, item_id):
     
     request.session['shopping_bag'] = shopping_bag
     return redirect(redirect_url)
+
+
+def modify_shopping_bag(request, item_id):
+    """ A view to modify the contents of the shopping bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if quantity > 0:
+        shopping_bag[item_id] = quantity
+    else:
+        shopping_bag.pop(item_id)
+    
+    request.session['shopping_bag'] = shopping_bag
+    return redirect(reverse('view_shopping_bag'))
+
+
+def remove_from_shopping_bag(request, item_id):
+    """ A view remove an item from the shopping bag"""
+
+    shopping_bag = request.session.get('shopping_bag', {})
+    shopping_bag.pop(item_id)
+
+    request.session['shopping_bag'] = shopping_bag
+    return HttpResponse(status=200)
