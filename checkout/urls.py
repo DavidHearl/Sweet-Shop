d62e53542ae25c557a462a4f120f426680d68880
DavidHearl <david.hearl@outlook.com>
2022-07-28 16:23:31 +0100

    Added Webhook functionality

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_completed_successfully/<order_number>', views.checkout_completed_successfully, name='checkout_completed_successfully'),
+    path('wh/', webhook, name='webhook'),
 ]
