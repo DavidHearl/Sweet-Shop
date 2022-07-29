aa8c9b66f55937ed3a4f44a4e9dc57990fe05638
DavidHearl <david.hearl@outlook.com>
2022-07-29 21:02:05 +0000

    changed allowed hosts

--- sweet_shop/settings.py
+++ sweet_shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['the-sweet-shop-davidhearl.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['https://the-sweet-shop-davidhearl.herokuapp.com', 'localhost']
 
 # Application definition
 
