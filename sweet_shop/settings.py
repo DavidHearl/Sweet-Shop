a4b9a004a605873e304a01bc57461855f3479e54
DavidHearl <david.hearl@outlook.com>
2022-07-29 20:35:44 +0000

    Removed secret key and set debug

--- sweet_shop/settings.py
+++ sweet_shop/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)wrovkj0h76t3e)@s3(rpswnc$bf&sz*_5d91kbvb4dw40y@q!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-sweet-shop-davidhearl.heroku.com', 'localhost']
 
