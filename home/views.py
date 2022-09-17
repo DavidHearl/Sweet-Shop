17acfcb4724960c3a8c38b6170f12ec55d3a589c
DavidHearl <david.hearl@outlook.com>
2022-09-17 15:58:55 +0100

    added carousel to home page

--- home/views.py
+++ home/views.py
@@ -6,5 +6,23 @@ def index(request):
     """ A view to return the index page """
 
     products = Product.objects.all()
-    
-    return render(request, 'home/index.html', {'products': products})
+    featured_count = 0
+    featured = 0
+
+    if request.GET:
+        for product in products:
+            if product.featured:
+                featured_count += 1
+
+            return featured_count
+
+    # context['featured'] = range[1,3]
+
+    context = {
+        'loop_times' : range(1,3),
+        'featured_count': featured_count,
+        'featured': featured,
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
