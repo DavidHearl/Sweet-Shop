202084d10e473892dfa34fc42505aa5137c82f08
DavidHearl <david.hearl@outlook.com>
2022-09-25 00:10:09 +0100

    Added a new carousel feature using flickety, removed old logic in views.py and updated styles

--- home/views.py
+++ home/views.py
@@ -6,18 +6,8 @@ def index(request):
     """ A view to return the index page """
 
     products = Product.objects.filter(featured=True)
-    carousel_list = []
-    featured_count = []
-
-    for product in products:
-        featured_count.append(product)
-        if len(featured_count) == 4:
-            carousel_list.append(featured_count[:])
-            featured_count.clear()
 
     context = {
-        'carousel_list': carousel_list,
-        'featured_count': featured_count,
         'products': products,
     }
 
